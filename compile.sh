c87a7541ac45e6a642b5e565a79ec77592d13050
Joey <jjiemjitpolchai9540@bths.edu>
2019-05-29 18:16:57 -0400

    compiling script

---
+++ compile.sh
@@ -0,0 +1,8 @@
+gcc=../../i686-elf-4.9.1-Linux-x86_64/bin/i686-elf-gcc
+
+$gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+$gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+cp myos.bin isodir/boot/myos.bin
+grub-mkrescue -o myos.iso isodir
+qemu-system-i386 -cdrom myos.iso
+
