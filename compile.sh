f5fe7c7139505dc9564f6c86d2ee4ba71ffced28
Joey <jjiemjitpolchai9540@bths.edu>
2019-05-29 21:42:37 -0400

    clean up binaries

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 gcc=../../i686-elf-4.9.1-Linux-x86_64/bin/i686-elf-gcc
 
+rm myos.bin
+rm myos.iso
 $gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 $gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
 cp myos.bin isodir/boot/myos.bin
